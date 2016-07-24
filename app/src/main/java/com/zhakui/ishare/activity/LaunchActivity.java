05c2d5fd78854a1b4519970e9b8d5e51983efded
xavier <zhkui@zhkui.com>
2016-07-24 23:04:30 +0800

    new project

---
+++ app/src/main/java/com/zhakui/ishare/activity/LaunchActivity.java
@@ -0,0 +1,15 @@
+package com.zhakui.ishare.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhakui.ishare.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
